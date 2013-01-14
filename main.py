06fe620a23bf1443d9bfed339a5bf5c288879cf1
elvis4526 <elvis4526@gmail.com>
2013-01-14 17:40:35 -0500

    now the portage tree get synced, /proc and /dev get mounted in the chroot, you can edit configuration file at the end and it can build automatically the latest debian-sources with the binary useflag

--- main.py
+++ main.py
@@ -60,14 +60,14 @@ class Partition():
         print("At this point, all your parition should be mounted at the appropriate mountpoint")
 
 
-class Stage(Arch):
+class Stage():
             
     def get(self):
         self.arches = Arch().check()
         if self.arches == "x86-32bit":
             arches_list = ["amd64-k8_32", "athlon-xp", "atom_32", "core2_32", "generic_32", "i686", "pentium4"]
         if self.arches == "x86-64bit":
-            arches_list = ["amd64-k8", "amd64-k10", "atom_64", "core2_64", "corei7", "generic64"]
+            arches_list = ["amd64-k8", "amd64-k10", "atom_64", "core2_64", "corei7", "generic_64"]
         number = 0
         for stage3 in arches_list:
             print(number,"-", stage3)
@@ -77,6 +77,7 @@ class Stage(Arch):
         self.arches_list = arches_list
         print(self.arches, self.arches_list[int(self.optimi_number)])
         try:
+            print("Downloaded of the stage3 archive started...")
             urllib.request.urlretrieve("http://ftp.osuosl.org/pub/funtoo/funtoo-current/{0}/{1}/stage3-latest.tar.xz".format(self.arches, self.arches_list[int(self.optimi_number)]), filename="/mnt/gentoo/stage3-latest.tar.xz")
         except PermissionError:
             print("Are you root?")
@@ -85,3 +86,61 @@ class Stage(Arch):
     def extract(self, path="/mnt/gentoo/stage3-latest.tar.bz2"):
         stage3 = tarfile.open(name=path, mode='r|*')
         stage3.extractall(path="/mnt/gentoo")
+
+class Tree():
+    def get(self):
+        subprocess.call("emerge --sync", shell=True)
+
+class pre_chroot():
+    def proc():
+        subprocess.call("mount --bind /proc /mnt/gentoo/proc", shell=True)
+
+    def dev():
+        subprocess.call("mount --bind /dev /mnt/gentoo/dev", shell=True)
+
+
+# Funtoo
+class Configure():
+    def fstab():
+        subprocess.call("nano /etc/fstab", shell=True)
+
+    def localtime():
+        subprocess.call("nano /etc/localtime", shell=True)
+
+    def portage():
+        subprocess.call("nano /etc/make.conf", shell=True)
+
+    def keymaps():
+        subprocess.call("nano /etc/conf.d/keymaps", shell=True)
+
+    def clock():
+        subprocess.call("nano /etc/conf.d/hwclock", shell=True)
+
+    def root_passwd():
+        print("Configuring root password")
+        subprocess.call("passwd root", shell=True)
+
+    def bootloader():
+        asw = input("Do you want to emerge boot-update? (yes or no): ")
+        if "yes" == asw:
+            subprocess.call("emerge boot-update", shell=True)
+
+    def kernel():
+        print("I'll build the debian-sources kernel with the binary useflag for you")
+        print("Doing this requires 12GB of space in /var/tmp")
+        subprocess.call("df -h /var/tmp", shell=True)
+        input("Do you have enough place? If you have, just press enter. If you don't, exit the script with CTRL-C and deal with the kernel yourself!")
+        packageuse = open("/etc/portage/package.use", "a")
+        packageuse.write("# The following line was added by funinstall")
+        packageuse.write("sys-kernel/debian-sources binary")
+        
+            
+Partition().check()            
+Partition().mount()
+Stage().get()
+Stage().extract()
+pre_chroot().proc()
+pre_chroot().dev()
+os.chroot("/mnt/gentoo")
+Configure().fstab()
+Configure().portage()
